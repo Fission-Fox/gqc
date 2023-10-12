dcb7c32ebd4d61eb662a97e94c7d0b8f3e667efc
mubashir-786 <mubashirrazaimran@gmail.com>
2023-10-12 18:56:15 +0500

    fix: body background color added

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -2,7 +2,7 @@ import BackgroudImage from "@/assets/images/team/1.jpg";
 import TeamCard from "../TeamCard";
 export default function Team() {
   return (
-    <div className="w-[90%] md:w-[60%] m-auto flex flex-col gap-6 mt-36">
+    <div className="w-[90%] md:w-[60%] m-auto flex flex-col gap-6 mt-48">
       <div className="flex flex-col md:flex-row">
         <div className="w-full lg:w-1/2">
           <p className="text-[22px] font-[700] underline text-[#19315B]">
